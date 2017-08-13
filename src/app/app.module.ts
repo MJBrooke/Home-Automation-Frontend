32fa13b20076a08d5da4607c0d5bb14034f37b45
michael86 <michaelb5@discovery.co.za>
2017-08-13 15:07:46 +0200

    Added all basic components for showing connected components/actuators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ComponentsComponent } from './home-page/components/components.component';
+import { SensorsComponent } from './home-page/sensors/sensors.component';
+import { ActuatorsComponent } from './home-page/actuators/actuators.component';
+import { SensorComponent } from './home-page/sensor/sensor.component';
+import { ActuatorComponent } from './home-page/actuator/actuator.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    ComponentsComponent,
+    SensorsComponent,
+    ActuatorsComponent,
+    SensorComponent,
+    ActuatorComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
