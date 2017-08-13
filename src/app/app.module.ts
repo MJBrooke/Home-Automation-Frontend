b8624eaf466536147e172dcd812e2d190545b824
michael86 <michaelb5@discovery.co.za>
2017-08-13 19:37:16 +0200

    Built up all the basic HTML/Components that make up the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ComponentsComponent } from './home-page/components/components.component';
-import { SensorsComponent } from './home-page/sensors/sensors.component';
-import { ActuatorsComponent } from './home-page/actuators/actuators.component';
-import { SensorComponent } from './home-page/sensor/sensor.component';
-import { ActuatorComponent } from './home-page/actuator/actuator.component';
+import { SensorsComponent } from './home-page/components/sensors/sensors.component';
+import { ActuatorsComponent } from './home-page/components/actuators/actuators.component';
+import { SensorComponent } from './home-page/components/sensors/sensor/sensor.component';
+import { ActuatorComponent } from './home-page/components/actuators/actuator/actuator.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FlowsComponent } from './home-page/flows/flows.component';
+import { FlowComponent } from './home-page/flows/flow/flow.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     ActuatorsComponent,
     SensorComponent,
     ActuatorComponent,
-    NavbarComponent
+    NavbarComponent,
+    FlowsComponent,
+    FlowComponent
   ],
   imports: [
     BrowserModule
